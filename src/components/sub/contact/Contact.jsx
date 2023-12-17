b48fee94f302f45cf1d820edd0b62fa512dbf1ce
yuna <yuna744@gmail.com>
2023-12-18 01:40:51 +0900

    1218 카카오 객체 참조 객체로 옮겨 의존성 배열 제거하기

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -3,7 +3,7 @@ import Layout from "../../common/layout/Layout";
 import "./Contact.scss";
 
 export default function Contact() {
-  const { kakao } = window;
+  const kakao = useRef(window.kakao);
   const [Index, setIndex] = useState(0);
   const mapFrame = useRef(null);
   const marker = useRef(null);
@@ -13,31 +13,37 @@ export default function Contact() {
   const mapInfo = useRef([
     {
       title: "삼성역 코엑스",
-      latlng: new kakao.maps.LatLng(37.51100661425726, 127.06162026853143),
+      latlng: new kakao.current.maps.LatLng(
+        37.51100661425726,
+        127.06162026853143
+      ),
       imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
-      imgSize: new kakao.maps.Size(232, 99),
-      imgPos: { offset: new kakao.maps.Point(116, 99) },
+      imgSize: new kakao.current.maps.Size(232, 99),
+      imgPos: { offset: new kakao.current.maps.Point(116, 99) },
     },
     {
       title: "넥슨 본사",
-      latlng: new kakao.maps.LatLng(37.40211707077346, 127.10344953763003),
+      latlng: new kakao.current.maps.LatLng(
+        37.40211707077346,
+        127.10344953763003
+      ),
       imgSrc: `${process.env.PUBLIC_URL}/img/marker2.png`,
-      imgSize: new kakao.maps.Size(232, 99),
-      imgPos: { offset: new kakao.maps.Point(116, 99) },
+      imgSize: new kakao.current.maps.Size(232, 99),
+      imgPos: { offset: new kakao.current.maps.Point(116, 99) },
     },
     {
       title: "서울 시청",
-      latlng: new kakao.maps.LatLng(37.5662952, 126.9779451),
+      latlng: new kakao.current.maps.LatLng(37.5662952, 126.9779451),
       imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
-      imgSize: new kakao.maps.Size(232, 99),
-      imgPos: { offset: new kakao.maps.Point(116, 99) },
+      imgSize: new kakao.current.maps.Size(232, 99),
+      imgPos: { offset: new kakao.current.maps.Point(116, 99) },
     },
   ]);
 
   //마커 인스턴스 생성
-  marker.current = new kakao.maps.Marker({
+  marker.current = new kakao.current.maps.Marker({
     position: mapInfo.current[Index].latlng,
-    image: new kakao.maps.MarkerImage(
+    image: new kakao.current.maps.MarkerImage(
       mapInfo.current[Index].imgSrc,
       mapInfo.current[Index].imgSize,
       mapInfo.current[Index].imgOpt
@@ -45,7 +51,7 @@ export default function Contact() {
   });
 
   useEffect(() => {
-    const mapInstance = new kakao.maps.Map(mapFrame.current, {
+    const mapInstance = new kakao.current.maps.Map(mapFrame.current, {
       center: mapInfo.current[Index].latlng,
       level: 3,
     });
