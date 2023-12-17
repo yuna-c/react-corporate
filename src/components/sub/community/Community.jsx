f550df2e6da0026bf8a545b2b763dbeb1c91aebc
yuna <yuna744@gmail.com>
2023-12-17 20:55:32 +0900

    1217 시간값 출력

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useRef, useState } from "react";
+import { useCustomText } from "../../../hooks/useText";
 import Layout from "../../common/layout/Layout";
 import "./Community.scss";
 
 export default function Community() {
   const path = useRef(process.env.PUBLIC_URL);
+  // const customText = useCustomText("combined");
+  const customText = useCustomText("combined");
+  // console.log(customText);
 
   const getLocalData = () => {
     const data = localStorage.getItem("post");
@@ -11,6 +15,7 @@ export default function Community() {
     // JSON.parse 문자열의 구문을 분석, Js 값이나 객체를 생성
     else return [];
   };
+
   const [Post, setPost] = useState(getLocalData()); //핸들링 위한 state
   const refTit = useRef(null);
   const refEmail = useRef(null);
@@ -39,15 +44,22 @@ export default function Community() {
     ) {
       resetPost();
     }
+    const korTime = new Date().getTime() + 1000 * 60 * 60 * 9;
     setPost([
       {
         title: refTit.current.value,
         email: refEmail.current.value,
         content: refCon.current.value,
+        date: new Date(korTime),
       },
       ...Post,
     ]);
     // resetPost();
+  };
+
+  const deletePost = (delIndex) => {
+    setPost(Post.filter((_, idx) => delIndex !== idx));
+    // filter(callbackFn, thisArg) : 메서드는 주어진 배열의 일부에 대한 얕은 복사본을 생성, 주어진 배열에서 제공된 함수에 의해 구현된 테스트를 통과한 요소로만 필터링
   };
 
   const handleSizeHeight = () => {
@@ -129,6 +141,7 @@ export default function Community() {
               type="text"
               placeholder="Your Title"
               name="title"
+              required="required"
               ref={refTit}
             />
             <label>Email</label>
@@ -136,6 +149,7 @@ export default function Community() {
               type="email"
               placeholder="Your Email"
               name="email"
+              required="required"
               ref={refEmail}
             />
             <label>Message</label>
@@ -144,6 +158,7 @@ export default function Community() {
               rows={15}
               maxLength={"1500"}
               name="content"
+              required="required"
               ref={refCon}
               onChange={handleSizeHeight}
             ></textarea>
@@ -164,17 +179,26 @@ export default function Community() {
 
         <div className="showBox">
           {Post.map((el, idx) => {
+            const date = JSON.stringify(el.date);
+            const strDate = customText(date.split("T")[0].slice(1), ".");
+            const strDate2 = customText(date.split("Z")[0].slice(12), ":");
+            const time = Math.floor(strDate2);
+            console.log(strDate2);
+
             return (
               <article key={el + idx}>
                 <div className="txt-area">
                   <h6>{el.title}</h6>
                   <strong>{el.email}</strong>
                   <p>{el.content}</p>
+                  <span>{strDate}</span>
                 </div>
 
                 <div className="btn-area">
                   <button className="btn">Edit</button>
-                  <button className="btn">Delete</button>
+                  <button className="btn" onClick={() => deletePost(idx)}>
+                    Delete
+                  </button>
                 </div>
 
                 <div className="line-holizontal"></div>
@@ -183,8 +207,6 @@ export default function Community() {
           })}
         </div>
       </div>
-
-      <div className="line-holizontal"></div>
     </Layout>
   );
 }
--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,54 +1,54 @@
-import { useCustomText } from '../../../hooks/useText';
-import Layout from '../../common/layout/Layout';
-import './Youtube.scss';
-import { useState, useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
+import { useCustomText } from "../../../hooks/useText";
+import Layout from "../../common/layout/Layout";
+import "./Youtube.scss";
+import { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
 
 export default function Youtube() {
-	const btnData = ['VIDEO', 'BRANDING', 'DESIGN', 'CONTENT'];
-	const customText = useCustomText('combined');
-	const shortenText = useCustomText('shorten');
-	const path = useRef(process.env.PUBLIC_URL);
-	const [Vids, setVids] = useState([]);
-	const [btnOn, setbtnOn] = useState('');
+  const btnData = ["VIDEO", "BRANDING", "DESIGN", "CONTENT"];
+  const customText = useCustomText("combined");
+  const shortenText = useCustomText("shorten");
+  const path = useRef(process.env.PUBLIC_URL);
+  const [Vids, setVids] = useState([]);
+  const [btnOn, setbtnOn] = useState("");
 
-	const fetchYoutube = async () => {
-		// const api_key = 'AIzaSyBgRldfomRBMNoipsSTKYAmfOarH1iIu8o';
-		// const pid = 'PL_gXk6OSOQ5LVWytUDP2MgKhA1-A5h1TJ';
-		const api_key = process.env.REACT_APP_YOUTUBE_API;
-		const pid = process.env.REACT_APP_YOUTUBE_LIST;
+  const fetchYoutube = async () => {
+    // const api_key = 'AIzaSyBgRldfomRBMNoipsSTKYAmfOarH1iIu8o';
+    // const pid = 'PL_gXk6OSOQ5LVWytUDP2MgKhA1-A5h1TJ';
+    const api_key = process.env.REACT_APP_YOUTUBE_API;
+    const pid = process.env.REACT_APP_YOUTUBE_LIST;
 
-		const num = 10;
-		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
-		try {
-			const data = await fetch(baseURL);
-			const json = await data.json();
-			setVids(json.items);
-		} catch (err) {
-			console.error(err);
-		}
-	};
+    const num = 10;
+    const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+    try {
+      const data = await fetch(baseURL);
+      const json = await data.json();
+      setVids(json.items);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-	const toggleBtn = (e) => {
-		setbtnOn((btn) => {
-			return e.target.value;
-		});
-	};
-	// console.log(btnData.values);
-	// console.log(toggleBtn);
+  const toggleBtn = (e) => {
+    setbtnOn((btn) => {
+      return e.target.value;
+    });
+  };
+  // console.log(btnData.values);
+  // console.log(toggleBtn);
 
-	useEffect(() => {
-		fetchYoutube();
-	}, []);
+  useEffect(() => {
+    fetchYoutube();
+  }, []);
 
-	return (
-		/* S : Youtube */
-		<Layout title={'Youtube'}>
-			<div className='visualBox'>
-				<h3>We create your brand together.</h3>
+  return (
+    /* S : Youtube */
+    <Layout title={"Youtube"}>
+      <div className="visualBox">
+        <h3>We create your brand together.</h3>
 
-				<div className='sort-area'>
-					{/*
+        <div className="sort-area">
+          {/*
 				많은 클래스는 어케 로직 짤지 이해해보기
 				<div className='btn-area'>
 					<div className='btn-inner'>
@@ -74,131 +74,186 @@ export default function Youtube() {
 						<button className='btn-active btn-inner-text-hover'>CONTENT</button>
 					</div>
 				</div> */}
-					{/* <button className='btn'>BRANDING</button>
+          {/* <button className='btn'>BRANDING</button>
 				<button className='btn'>DESIGN</button>
 				<button className='btn'>VIDEO</button>
 				<button className='btn'>CONTENT</button> */}
-					{btnData.map((item, idx) => {
-						return (
-							<button value={idx} key={item + idx} className={`btn ${idx == btnOn ? 'on' : ''}`} onClick={toggleBtn}>
-								{item}
-							</button>
-						);
-					})}
-				</div>
-			</div>
+          {btnData.map((item, idx) => {
+            return (
+              <button
+                value={idx}
+                key={item + idx}
+                className={`btn ${idx == btnOn ? "on" : ""}`}
+                onClick={toggleBtn}
+              >
+                {item}
+              </button>
+            );
+          })}
+        </div>
+      </div>
 
-			<div className='line-holizontal'></div>
+      <div className="line-holizontal"></div>
 
-			<section className='videoBox'>
-				<div className='txt-area'>
-					<div className='icon'>
-						<img src={`${path.current}/img/icon03.svg`} alt='icon03.svg' />
-					</div>
-					<h4>VIDEO</h4>
-					<p>
-						VIDEOS ADS <br></br>SOCIAL MEDIA SHORTS <br></br>MOVIE TRAILERS
-					</p>
+      <section className="videoBox">
+        <div className="txt-area">
+          <div className="icon">
+            <img src={`${path.current}/img/icon03.svg`} alt="icon03.svg" />
+          </div>
+          <h4>VIDEO</h4>
+          <p>
+            VIDEOS ADS <br></br>SOCIAL MEDIA SHORTS <br></br>MOVIE TRAILERS
+          </p>
 
-					{/* <button className='btn'>GET IN TOUCH</button> */}
-					<div className='btn-area'>
-						<div className='btn-inner'>
-							<button className='btn-active btn-inner-text'>GET IN TOUCH</button>
-							<button className='btn-active btn-inner-text-hover'>GET IN TOUCH</button>
-						</div>
-					</div>
-				</div>
+          {/* <button className='btn'>GET IN TOUCH</button> */}
+          <div className="btn-area">
+            <div className="btn-inner">
+              <button className="btn-active btn-inner-text">
+                GET IN TOUCH
+              </button>
+              <button className="btn-active btn-inner-text-hover">
+                GET IN TOUCH
+              </button>
+            </div>
+          </div>
+        </div>
 
-				<div className='con-area'>
-					<div className='line-vertical'></div>
+        <div className="con-area">
+          <div className="line-vertical"></div>
 
-					<div className='info'>
-						<div className='txt'>
-							<h5>We inspire people to stand up for what they believe in with amazing video strategies.</h5>
-							<p>
-								With our exceptional video strategies, we ignite inspiration and empower individuals to champion their beliefs fearlessly. Through visually captivating storytelling and impactful
-								messaging, we spark a call to action that resonates deeply. Our expert team blends creativity and strategy to create compelling video content that drives engagement and provokes
-								meaningful conversations. Whether it's a thought-provoking documentary, an emotionally charged campaign, or a powerful brand story, we craft narratives that leave a lasting impact.
-								Together, let's harness the power of video to inspire, motivate, and mobilize audiences towards positive change and a brighter future.
-							</p>
-						</div>
+          <div className="info">
+            <div className="txt">
+              <h5>
+                We inspire people to stand up for what they believe in with
+                amazing video strategies.
+              </h5>
+              <p>
+                With our exceptional video strategies, we ignite inspiration and
+                empower individuals to champion their beliefs fearlessly.
+                Through visually captivating storytelling and impactful
+                messaging, we spark a call to action that resonates deeply. Our
+                expert team blends creativity and strategy to create compelling
+                video content that drives engagement and provokes meaningful
+                conversations. Whether it's a thought-provoking documentary, an
+                emotionally charged campaign, or a powerful brand story, we
+                craft narratives that leave a lasting impact. Together, let's
+                harness the power of video to inspire, motivate, and mobilize
+                audiences towards positive change and a brighter future.
+              </p>
+            </div>
 
-						<div className='video-example'>
-							<video src={`${path.current}/img/services-video-transcode.mp4`} alt='services-content-transcode' autoPlay muted loop playsInline />
-						</div>
+            <div className="video-example">
+              <video
+                src={`${path.current}/img/services-video-transcode.mp4`}
+                alt="services-content-transcode"
+                autoPlay
+                muted
+                loop
+                playsInline
+              />
+            </div>
 
-						<div className='line-holizontal'></div>
+            <div className="line-holizontal"></div>
 
-						<div className='video-area'>
-							{Vids.map((data) => {
-								const [date, time] = data.snippet.publishedAt.split('T');
-								return (
-									<article key={data.id}>
-										<h6>{shortenText(data.snippet.title, 40)}</h6>
+            <div className="video-area">
+              {Vids.map((data) => {
+                const [date, time] = data.snippet.publishedAt.split("T");
+                return (
+                  <article key={data.id}>
+                    <h6>{shortenText(data.snippet.title, 40)}</h6>
 
-										<div className='pic'>
-											<Link to={`/detail/${data.id}`}>
-												<img src={data.snippet.thumbnails.standard ? data.snippet.thumbnails.standard.url : '/img/member1.jpg'} alt={data.snippet.title} />
-											</Link>
-										</div>
+                    <div className="pic">
+                      <Link to={`/detail/${data.id}`}>
+                        <img
+                          src={
+                            data.snippet.thumbnails.standard
+                              ? data.snippet.thumbnails.standard.url
+                              : "/img/member1.jpg"
+                          }
+                          alt={data.snippet.title}
+                        />
+                      </Link>
+                    </div>
 
-										<div className='txt'>
-											<p>{shortenText(data.snippet.description, 180)}</p>
-											<div className='infoBox'>
-												<span>{customText(date, '.')}</span>
-												<em>{time.split('Z')[0]}</em>
-											</div>
-										</div>
-									</article>
-								);
-							})}
-						</div>
-					</div>
-				</div>
-			</section>
+                    <div className="txt">
+                      <p>{shortenText(data.snippet.description, 180)}</p>
+                      <div className="infoBox">
+                        <span>{customText(date, ".")}</span>
+                        <em>{time.split("Z")[0]}</em>
+                      </div>
+                    </div>
+                  </article>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
 
-			<div className='line-holizontal'></div>
+      <div className="line-holizontal"></div>
 
-			<section className='brandingBox'>
-				<div className='txt-area'>
-					<div className='icon'>
-						<img src={`${path.current}/img/icon01.svg`} alt='icon01.svg' />
-					</div>
-					<h4>branding</h4>
-					<p>
-						LOGO<br></br>CORPORATE IDENTITY<br></br>BRAND GUIDE
-					</p>
+      <section className="brandingBox">
+        <div className="txt-area">
+          <div className="icon">
+            <img src={`${path.current}/img/icon01.svg`} alt="icon01.svg" />
+          </div>
+          <h4>branding</h4>
+          <p>
+            LOGO<br></br>CORPORATE IDENTITY<br></br>BRAND GUIDE
+          </p>
 
-					{/* <button className='btn'>GET IN TOUCH</button> */}
-					<div className='btn-area'>
-						<div className='btn-inner'>
-							<button className='btn-active btn-inner-text'>GET IN TOUCH</button>
-							<button className='btn-active btn-inner-text-hover'>GET IN TOUCH</button>
-						</div>
-					</div>
-				</div>
+          {/* <button className='btn'>GET IN TOUCH</button> */}
+          <div className="btn-area">
+            <div className="btn-inner">
+              <button className="btn-active btn-inner-text">
+                GET IN TOUCH
+              </button>
+              <button className="btn-active btn-inner-text-hover">
+                GET IN TOUCH
+              </button>
+            </div>
+          </div>
+        </div>
 
-				<div className='con-area'>
-					<div className='line-vertical'></div>
+        <div className="con-area">
+          <div className="line-vertical"></div>
 
-					<div className='info'>
-						<div className='txt'>
-							<h5>A strong brand is a tool to connect people with your brand all around the world.</h5>
-							<p>
-								A strong brand serves as a powerful conduit, transcending borders and bringing people from all corners of the globe together. It acts as a universal language, fostering connections,
-								and forging lasting relationships. By crafting a compelling brand identity, we equip you with the tools to resonate with diverse audiences, irrespective of geographical boundaries. Our
-								strategic approach ensures that your brand story is authentically communicated, captivating hearts and minds across cultures. Let us guide you in harnessing the true potential of your
-								brand, bridging gaps, and leaving an indelible mark on a global scale. Together, we can connect the world to your brand's unique essence.
-							</p>
-						</div>
+          <div className="info">
+            <div className="txt">
+              <h5>
+                A strong brand is a tool to connect people with your brand all
+                around the world.
+              </h5>
+              <p>
+                A strong brand serves as a powerful conduit, transcending
+                borders and bringing people from all corners of the globe
+                together. It acts as a universal language, fostering
+                connections, and forging lasting relationships. By crafting a
+                compelling brand identity, we equip you with the tools to
+                resonate with diverse audiences, irrespective of geographical
+                boundaries. Our strategic approach ensures that your brand story
+                is authentically communicated, captivating hearts and minds
+                across cultures. Let us guide you in harnessing the true
+                potential of your brand, bridging gaps, and leaving an indelible
+                mark on a global scale. Together, we can connect the world to
+                your brand's unique essence.
+              </p>
+            </div>
 
-						<div className='video-example'>
-							<video src={`${path.current}/img/services-branding-transcode.mp4`} alt='services-content-transcode' autoPlay muted loop playsInline />
-						</div>
-					</div>
-				</div>
-			</section>
-		</Layout>
-		/* //E : Youtube */
-	);
+            <div className="video-example">
+              <video
+                src={`${path.current}/img/services-branding-transcode.mp4`}
+                alt="services-content-transcode"
+                autoPlay
+                muted
+                loop
+                playsInline
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+    /* //E : Youtube */
+  );
 }
