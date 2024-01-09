eed6dc4f3feed7cc2ec42d12605d498ffd603cb8
yuna <yuna744@gmail.com>
2024-01-10 03:06:53 +0900

    0110 메인 페이지 완료

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,5 +1,54 @@
+import { useEffect, useRef, useState } from 'react';
+import { useCustomText } from '../../../hooks/useText';
 import './Visual.scss';
+import { Link } from 'react-router-dom';
 
 export default function Visual() {
-	return /* S : Visual */ <figure className='Visual'>Visual</figure>; /* E : Visual */
+	const customText = useCustomText('combined');
+	const shortenText = useCustomText('shorten');
+	const [Vids, setVids] = useState([]);
+
+	const fetchYoutube = async () => {
+		const api_key = process.env.REACT_APP_YOUTUBE_API;
+		const pid = process.env.REACT_APP_YOUTUBE_LIST;
+
+		const num = 14;
+		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+		try {
+			const data = await fetch(baseURL);
+			const json = await data.json();
+			setVids(json.items);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	useEffect(() => {
+		fetchYoutube();
+	}, []);
+
+	return (
+		/* S : Visual */
+		<figure className='Visual'>
+			<div className='visualBox'>
+				{Vids.message ? (
+					<h1>{Vids.message}</h1>
+				) : (
+					Vids.map((vid, idx) => {
+						if (idx >= 6) return null;
+						return (
+							<article key={vid.id}>
+								<div className='pic'>
+									<Link to={`/detail/${vid.id}`}>
+										<img src={vid.snippet.thumbnails.default.url} alt={vid.snippet.title} />
+									</Link>
+								</div>
+							</article>
+						);
+					})
+				)}
+			</div>
+		</figure>
+		/* E : Visual */
+	);
 }
--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -2,7 +2,7 @@ import { AnimatePresence, motion } from 'framer-motion';
 import { useEffect, useRef, useState } from 'react';
 import { useCustomText } from '../../../hooks/useText';
 import Layout from '../../common/layout/Layout';
-import postData from './dummyPosts.json';
+import postData from '../../main/info/dummyPosts.json';
 import './Community.scss';
 
 export default function Community() {
@@ -14,7 +14,7 @@ export default function Community() {
 		const data = localStorage.getItem('post');
 		// JSON.parse 문자열의 구문을 분석, Js 값이나 객체를 생성
 		if (data) JSON.parse(data);
-		return postData.dummyPosts; //[]빈배열에 json 파일 넣기
+		return postData.dummyPosts; //info []빈배열에 json 파일 넣기
 	};
 	// console.log(getLocalData());
 
