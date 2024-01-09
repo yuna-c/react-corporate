ee15851367e4db4ff46e70e8a1720976e234eabe
yuna <yuna744@gmail.com>
2024-01-09 11:11:11 +0900

    0109 모바일 메뉴 disabled 문제 해결

--- src/components/common/menu/Menu.jsx
+++ src/components/common/menu/Menu.jsx
@@ -5,19 +5,28 @@ import Mobile from '../mobile/Mobile';
 // npm i framer-motion@4
 
 export default function Menu({ setToggle, Dark, setDark }) {
-	const closeMenu = useCallback(() => {
-		// e.preventDefault();
+	const closeMenu = useCallback(
+		e => {
+			e.preventDefault();
+			window.innerWidth >= 1200 && setToggle(false);
+		},
+		[setToggle]
+	);
+
+	const disable = useCallback(() => {
 		const disable = document.getElementsByClassName('wrapper');
-		window.innerWidth >= 1200 && disable && setToggle(false);
+		disable && setToggle(false);
 	}, [setToggle]);
 
 	useEffect(() => {
 		window.addEventListener('resize', closeMenu);
+		window.addEventListener('click', disable);
 
 		return () => {
 			window.removeEventListener('resize', closeMenu);
+			window.removeEventListener('click', disable);
 		};
-	}, [closeMenu]);
+	}, [closeMenu, disable]);
 
 	return (
 		/* S : Menu */
